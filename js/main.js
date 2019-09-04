ac4ba51fab96b9e16ea800ce8b4fc20de1983641
ZeyuKeithFu <zeyufu@bu.edu>
2019-09-03 21:04:04 -0400

    Add player object.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,72 @@
-let { init, Sprite, GameLoop } = kontra;
-let { canvas } = init();
-
-let DARK_COLOR = '#333333';
-let LIGHT_COLOR = '#ffffff';
+let { init, getContext, Sprite, GameLoop } = kontra;
+let { canvas, context } = init();
 
 /**
- * player setup
+ * Game params
+ */
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
+let HALF_WIDTH = canvas.width / 2;
+let HALF_HEIGHT = canvas.height / 2;
+let DARK_COLOR = '#333333';
+let LIGHT_COLOR = '#ffffff';
+let upmode = true;
+let grounded = true;
+
+/**
+ * Background setup
+ */
+function drawBackground() {
+    let ctx = getContext();
+    ctx.beginPath();
+    ctx.moveTo(0, HALF_HEIGHT);
+    ctx.lineTo(canvas.width, HALF_HEIGHT);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = DARK_COLOR;
+    ctx.stroke();
+}
+
+/**
+ * Player setup
  */
 let player = Sprite({
-    x: canvas.width / 2,
-    y: canvas.height / 2,
+    x: HALF_WIDTH,
+    y: HALF_HEIGHT,
     anchor: {x: 0.5, y: 1},
     width: 20,
     height: 20,
-    color: DARK_COLOR
-});
-
-window.addEventListener("keydown", function(e) {
-    if (e.code == "ArrowUp") {
-        player.dy = -10;
-        player.ddy = 0.5;
+    color: DARK_COLOR,
+    checkPos: function() {
+        if (
+            (upmode && player.y > HALF_HEIGHT) ||
+            (!upmode && player.y < HALF_HEIGHT)) {
+            player.y = HALF_HEIGHT;
+            player.dy = 0;
+            player.ddy = 0;
+            grounded = true;
+        }
     }
 });
 
-/**
- * 
- */
-
+window.addEventListener("keydown", function(e) {
+    // jump up
+    if (upmode && e.code == "ArrowUp") {
+        grounded = false;
+        player.dy = -10;
+        player.ddy = 0.5;
+    }
+    // jump down
+    if (!upmode && e.code == "ArrowDown") {
+        grounded = false;
+        player.dy = 10;
+        player.ddy = -0.5;
+    }
+    // go backside
+    if (grounded && e.code == "Space") {
+        upmode = !upmode;
+        player.anchor.y = 1 - player.anchor.y;
+    }
+});
 
 /**
  * Game loop
@@ -34,9 +74,11 @@ window.addEventListener("keydown", function(e) {
 let loop = GameLoop({
     update: function() {
         player.update();
+        player.checkPos();
     },
     render: function() {
         player.render();
+        drawBackground();
     }
 });
 
